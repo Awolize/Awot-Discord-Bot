73106faa912b73afc1b5c177fe8fd3df5f36dae3
Alexander Eriksson <aleer778@student.liu.se>
2020-01-08 23:15:53 +0100

    Regenerated leaked info, production database up on debian 9 (BuyVM) and a example config file was created.

---
+++ example_config.py
@@ -0,0 +1,14 @@
+# Bot token
+DISCORD_TOKEN = ""
+
+# Bot prefix
+DISCORD_PREFIX = "<"
+
+# Time of day to print birthdays in chat
+BIRTHDAY_PRINT_TIME = '00:00:00'
+
+# Database postgreSQL
+DATABASE_DATABASE = "Awot"
+DATABASE_USER = "postgres"
+DATABASE_PASSWORD = ""
+DATABASE_TIMEOUT = 60
